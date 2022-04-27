24ae3a5415922c11e3f8692630c2a1188f2baf0a
paolavargasesd <engerlinsilva@gmail.com>
2022-04-27 18:38:27 +0200

    js

---
+++ variables #1/index.js
@@ -0,0 +1,28 @@
+/* ejercicio 1 */
+var myFavoriteHero;
+myFavoriteHero = "Hulk";
+
+console.log(myFavoriteHero);
+
+/* ejercicio 2 */
+
+var x;
+x = 50;
+
+console.log(x);
+
+/* numero 3 */
+
+var h;
+var y;
+
+h = 5;
+y = 10;
+
+console.log(h);
+console.log(y);
+
+/* numero 4 */
+
+var z = h + y;
+console.log (z);
